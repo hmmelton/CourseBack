5b444c1699541ec00cc3c2fdce6e1d6bf71db05b
Harrison Melton <hmmelton95@gmail.com>
2015-09-14 18:25:32 -0700

    added helper class dedicated to authentication

---
+++ app/src/main/java/com/hmmelton/textrack/utils/Authentication.java
@@ -0,0 +1,27 @@
+package com.hmmelton.textrack.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.hmmelton.textrack.SignInActivity;
+import com.parse.ParseUser;
+
+/**
+ * Created by harrisonmelton on 9/14/15.
+ */
+public class Authentication {
+
+    /**
+     * This method checks whether or not the user is signed in.  If the user is not signed in, s/he
+     * will be directed to the sign in page.
+     */
+    public static void isUserSignedIn(Activity activity) {
+        if (ParseUser.getCurrentUser() == null) {
+            Intent intent = new Intent(activity, SignInActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            activity.startActivity(intent);
+            activity.finish(); // removes activity from backstack
+            activity.overridePendingTransition(0,0); // used to prevent animation between activities
+        }
+    }
+}

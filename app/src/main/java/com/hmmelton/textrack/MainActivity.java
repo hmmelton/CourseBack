7b89f5ba7e398234b7d8c7bcac83b5f5766e5f31
Harrison Melton <hmmelton95@gmail.com>
2015-09-14 21:14:06 -0700

    removed methods involving menu

--- app/src/main/java/com/hmmelton/textrack/MainActivity.java
+++ app/src/main/java/com/hmmelton/textrack/MainActivity.java
@@ -1,18 +1,11 @@
 package com.hmmelton.textrack;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
 
-import com.parse.ParseUser;
+import com.hmmelton.textrack.utils.Authentication;
 
 import com.hmmelton.textrack.fragments.MainFragment;
 import com.hmmelton.textrack.fragments.NavigationDrawerFragment;
@@ -26,13 +19,6 @@ public class MainActivity extends AppCompatActivity
      */
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-
-    private ProgressBar mProgressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,85 +26,24 @@ public class MainActivity extends AppCompatActivity
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
 
-        isUserSignedIn();
+        Authentication.isUserSignedIn(this);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commit();
-
-        mProgressBar = (ProgressBar) findViewById(R.id.main_progress_bar);
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing com.hmmelton.textrack.fragments
 
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        switch (id) {
-            case R.id.action_settings:
-                break;
-            case R.id.action_logout:
-                mProgressBar.setVisibility(View.VISIBLE);
-                ParseUser.logOut();
-                isUserSignedIn();
-                break;
-            default:
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * This method checks whether or not the user is signed in.  If the user is not signed in, s/he
-     * will be directed to the sign in page.
-     */
-    private void isUserSignedIn() {
-        if (ParseUser.getCurrentUser() == null) {
-            Intent intent = new Intent(this, SignInActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            startActivity(intent);
-            finish(); // removes activity from backstack
-            overridePendingTransition(0,0); // used to prevent animation between activities
-        }
     }
 
 }

6f6bfbb908060865725c6bca3bc996423be43e7f
Harrison Melton <hmmelton95@gmail.com>
2016-05-07 14:54:38 -0700

    finished User model

--- app/src/main/java/com/hmmelton/courseback/models/User.java
+++ app/src/main/java/com/hmmelton/courseback/models/User.java
@@ -2,6 +2,41 @@ package com.hmmelton.courseback.models;
 
 /**
  * Created by harrisonmelton on 5/7/16.
+ * Each instance of this class holds information of one user.
  */
 public class User {
+
+    private String id; // user ID
+    private String name; // user's full name
+    private String email; // user's email address
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    /**
+     * This method returns the user's ID.
+     * @return user's id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * This method returns the user's name.
+     * @return user's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * This method returns the user's email address.
+     * @return
+     */
+    public String getEmail() {
+        return email;
+    }
 }

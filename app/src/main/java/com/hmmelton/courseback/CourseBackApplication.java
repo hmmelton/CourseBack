51d69f67df80367bc331732617167b7f06c6995d
Harrison Melton <hmmelton95@gmail.com>
2016-05-07 10:59:11 -0700

    added documentation to Application file

--- app/src/main/java/com/hmmelton/courseback/CourseBackApplication.java
+++ app/src/main/java/com/hmmelton/courseback/CourseBackApplication.java
@@ -9,9 +9,9 @@ import com.firebase.client.Firebase;
  */
 public class CourseBackApplication extends Application {
 
-    private static CourseBackApplication instance;
-    private static String fbId;
-    private static Firebase firebase;
+    private static CourseBackApplication instance; // global context
+    private static String fbId; // user's Facebook ID number
+    private static Firebase firebase; // instance of database
 
     @Override
     public void onCreate() {
@@ -23,14 +23,34 @@ public class CourseBackApplication extends Application {
         instance = this;
     }
 
+    /**
+     * This method returns the application's global context.
+     * @return global context
+     */
     public static CourseBackApplication getInstance() {
         return instance;
     }
 
+    /**
+     * This method returns the application's global database instance.
+     * @return instance of database
+     */
+    public static Firebase getDatabase() {
+        return firebase;
+    }
+
+    /**
+     * This method returns the user's Facebook ID number.
+     * @return Facebook ID number
+     */
     public static String getFacebookId() {
         return fbId;
     }
 
+    /**
+     * This method sets the user's Facebook ID number for future reference.
+     * @param id user's Facebook ID number
+     */
     public static void setFacebookId(String id) {
         fbId = id;
     }
